9483c8e60e5b4955b3860d48eab029d17a3d8e19
Sergey Suntsev <greyewi@gmail.com>
2024-05-31 11:06:41 +0500

    first task init

--- init.ts
+++ init.ts
@@ -1,1 +1,19 @@
-console.log("hello world!!")
+//Даны два числа a и b. Найти их среднее арифметическое: "(a + b)/2"
+
+{
+  let a = 3
+  let b = 5
+  console.log((a + b) /2)
+}
+
+
+// Дано двузначное число. Найти сумму и произведение его цифр.
+
+{
+  let n = 56
+
+  let first = Math.floor(n / 10)
+  let second = n % 10
+
+  console.log(first, second)
+}
